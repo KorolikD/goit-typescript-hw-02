a2fa32d2ac4354e76ad240e6384ce588a9a3d65b
Dmytro Korolik <dimytry.korolik@gmail.com>
2023-12-24 01:34:41 +0200

    some-fix

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -3,6 +3,26 @@
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
 
+// *Звернення до властивості через []
+// enum DaysOfWeek {
+//   monday,
+//   tuesday,
+//   wednesday,
+//   thursday,
+//   friday,
+//   saturday,
+//   sunday,
+// }
+
+// const isWeekend = (day: string): boolean => {
+//   const optimizedDay = day.toLocaleLowerCase().trim();
+//   return DaysOfWeek[DaysOfWeek.saturday] === optimizedDay ||
+//     DaysOfWeek[DaysOfWeek.sunday] === optimizedDay
+//     ? true
+//     : false;
+// };
+
+// *Повний опис властивостей
 enum DaysOfWeek {
   monday = "monday",
   tuesday = "tuesday",
@@ -13,8 +33,10 @@ enum DaysOfWeek {
   sunday = "sunday",
 }
 
-const isWeekend = (day: string): boolean =>
-  DaysOfWeek[DaysOfWeek.saturday] === day ||
-  DaysOfWeek[DaysOfWeek.sunday] === day
+const isWeekend = (day: string): boolean => {
+  const optimizedDay = day.toLocaleLowerCase().trim();
+  return DaysOfWeek.saturday === optimizedDay ||
+    DaysOfWeek.sunday === optimizedDay
     ? true
     : false;
+};
